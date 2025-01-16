ac7848b0071c122cd15c06b32b813c73f6647112
Justin Bird <justin@plug.vin>
2025-01-16 09:21:40 -0800

    add preinstall script

--- apps/hauler/src/actions/user.ts
+++ apps/hauler/src/actions/user.ts
@@ -3,7 +3,7 @@
 import { prisma } from "@repo/db";
 
 export async function submitAddUser(formData: FormData) {
-  return await prisma.user.create({
+  await prisma.user.create({
     data: {
       name: formData.get("name") as string,
       email: formData.get("email") as string,
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,2 @@
 export { prisma } from "./client"; // exports instance of prisma
-export * from "../generted/client"; // exports generated types from prisma
+export * from "../generated/client"; // exports generated types from prisma
