0406cd259467cd8fe950a4be19396ee2f08c1735
Justin Bird <justin@plug.vin>
2025-01-16 08:00:40 -0800

    moved client output

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,2 @@
 export { prisma } from "./client"; // exports instance of prisma
-export * from "@prisma/client"; // exports generated types from prisma
+export * from "../generted/client"; // exports generated types from prisma
