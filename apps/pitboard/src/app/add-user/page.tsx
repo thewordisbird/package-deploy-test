7da80613d96fb1e64f2216ee5dd9c4566ebc89ac
Justin Bird <justin@plug.vin>
2025-01-17 12:57:52 -0800

    make user page ssr

--- apps/pitboard/src/app/add-user/page.tsx
+++ apps/pitboard/src/app/add-user/page.tsx
@@ -1,5 +1,5 @@
 import AddUserForm from "./components/add-user-form";
 
-export default function Home() {
+export default function Page() {
   return <AddUserForm />;
 }
---
+++ apps/pitboard/src/app/user/page.tsx
@@ -0,0 +1,20 @@
+import { prisma } from "@repo/db";
+
+export default async function Page() {
+  const users = await prisma.user.findMany();
+
+  console.log("user", users);
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {users.map((user) => (
+          <div key={user.id} className="flex gap-4 items-center">
+            <span>{user.name}</span>
+            <span>{user.email}</span>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
