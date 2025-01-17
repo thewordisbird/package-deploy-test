6adb29ce9577857de7c1991932e70c420d4fc50a
Justin Bird <justin@plug.vin>
2025-01-17 12:17:10 -0800

    add user form

---
+++ apps/pitboard/src/app/add-user/page.tsx
@@ -0,0 +1,31 @@
+import { submitAddUser } from "@/actions/user";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <form action={submitAddUser} className="flex flex-col gap-2">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="text-black"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="my-email@email.com"
+            className="text-black"
+          />
+          <button
+            type="submit"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          >
+            Add User
+          </button>
+        </form>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    </div>
+  );
+}
