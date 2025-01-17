e3f1e4df0279ba5e7bca2bff9c2565d66ef3b4d6
Justin Bird <justin@plug.vin>
2025-01-17 12:47:49 -0800

    list all users

--- apps/pitboard/src/app/page.tsx
+++ apps/pitboard/src/app/page.tsx
@@ -1,14 +1,19 @@
 import { prisma } from "@repo/db";
 
 export default async function Home() {
-  const user = await prisma.user.findFirst();
+  const users = await prisma.user.findMany();
 
-  console.log("user", user);
+  console.log("user", users);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {user?.name ?? "No user added yet"}
+        {users.map((user) => (
+          <div key={user.id} className="flex gap-4 items-center">
+            <span>{user.name}</span>
+            <span>{user.email}</span>
+          </div>
+        ))}
       </main>
     </div>
   );
